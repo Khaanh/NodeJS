641067b367c6b2c7e17f9a3f3ac3a285800ab69b
Khanh <Khanh13Nguyen13@gmail.com>
2024-07-30 10:44:48 +0200

    Improve file structure

--- 4-natours/routes/userRoutes.js
+++ 4-natours/routes/userRoutes.js
@@ -1,43 +1,16 @@
 const express = require("express");
+const userController = require("../controllers/userController");
 
 const router = express.Router();
 
-const getAllUsers = (req, res) => {
-	res.status(500).json({
-		status: "error",
-		message: "This route is not yet defined",
-	});
-};
-
-const getUsers = (req, res) => {
-	res.status(500).json({
-		status: "error",
-		message: "This route is not yet defined",
-	});
-};
-
-const createUsers = (req, res) => {
-	res.status(500).json({
-		status: "error",
-		message: "This route is not yet defined",
-	});
-};
-
-const updateUsers = (req, res) => {
-	res.status(500).json({
-		status: "error",
-		message: "This route is not yet defined",
-	});
-};
-
-const deleteUsers = (req, res) => {
-	res.status(500).json({
-		status: "error",
-		message: "This route is not yet defined",
-	});
-};
-
-router.route("/").get(getAllUsers).post(createUsers);
-router.route("/:id").get(getUsers).patch(updateUsers).delete(deleteUsers);
+router
+	.route("/")
+	.get(userController.getAllUsers)
+	.post(userController.createUsers);
+router
+	.route("/:id")
+	.get(userController.getUsers)
+	.patch(userController.updateUsers)
+	.delete(userController.deleteUsers);
 
 module.exports = router;
