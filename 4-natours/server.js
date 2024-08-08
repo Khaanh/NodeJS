d22d15614743686f41aae93eb407a98af0f6518b
Khanh <Khanh13Nguyen13@gmail.com>
2024-08-08 14:29:08 +0200

    Importing development data

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,23 +1,4 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
 const app = require("./app");
-
-dotenv.config({ path: "./config.env" });
-
-const DB = process.env.DATABASE.replace(
-	"<password>",
-	process.env.DATABASE_PASSWORD
-);
-
-mongoose
-	.connect(DB, {
-		useNewUrlParser: true,
-		useCreateIndex: true,
-		useFindAndModify: false,
-	})
-	.then(() => {
-		console.log(`Connected to DB`);
-	});
 
 const PORT = process.env.PORT || 8800;
 app.listen(PORT, () => {
